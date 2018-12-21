9c952d8cdb286a9a3e2e4e96c87adcaadd6daa5e
yuanmeng <yuanmeng@gmail.com>
2018-12-21 09:52:20 +0800

    1

--- MyConn.go
+++ MyConn.go
@@ -6,13 +6,13 @@ import (
 )
 
 type MyConn struct {
-	Conn *net.Conn
-	Reader *bufio.Reader
-	Writer *bufio.Writer
+	Conn      *net.Conn
+	Reader    *bufio.Reader
+	Writer    *bufio.Writer
 	InputChan chan []byte
 }
 
-func (c *MyConn)start(){
+func (c *MyConn) start() {
 	go func() {
 		for n := range c.InputChan {
 			c.Writer.Write(n)
--- MyServer.go
+++ MyServer.go
@@ -3,33 +3,54 @@ package main
 import (
 	"bufio"
 	"net"
+	"strings"
 	"sync"
 )
 
 type MyServer struct {
-	Conns map[string]*MyConn
+	Conns  map[string]*MyConn
 	RwLock sync.RWMutex
 }
-func (s * MyServer) start(){
-	ln,_:=net.Listen("tcp","127.0.0.1:3003")
+
+func (s *MyServer) start() {
+	ln, _ := net.Listen("tcp", "127.0.0.1:3003")
 	defer ln.Close()
 	for {
-		conn,_ := ln.Accept()
-		reader:= bufio.NewReader(conn);
-		first,_ := reader.ReadString('\n')
-		writer:=bufio.NewWriter(conn)
-		myConn:=MyConn{
-			Conn:&conn,
-			Reader:reader,
-			Writer:writer,
-			InputChan:make(chan []byte),
+		conn, _ := ln.Accept()
+		reader := bufio.NewReader(conn)
+		first, _ := reader.ReadString('\n')
+		writer := bufio.NewWriter(conn)
+		myConn := &MyConn{
+			Conn:      &conn,
+			Reader:    reader,
+			Writer:    writer,
+			InputChan: make(chan []byte),
 		}
 		myConn.start()
 		s.RwLock.Lock()
-		s.Conns[first]=&myConn
+		s.Conns[first] = myConn
 		s.RwLock.Unlock()
+		go func() {
+			for {
+				msg, _ := myConn.Reader.ReadString('\n')
+				msgArr := strings.Split(msg, "::")
+				if len(msgArr) == 2 {
+					id := msgArr[0]
+					msgBody := msgArr[1]
+					s.RwLock.RLock()
+					target := s.Conns[id]
+					if target != nil {
+						target.InputChan <- []byte(msgBody)
+					}
+					s.RwLock.RUnlock()
+				}
+			}
+		}()
 	}
 }
 func main() {
-	
+	server := &MyServer{
+		Conns: make(map[string]*MyConn),
+	}
+	server.start()
 }
